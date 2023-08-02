2eabd61e8248539d4acbcf3ce8c5c6d63905b43c
Yaboisire <Yaboisire2gmail.com>
2023-08-02 19:23:19 +0300

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Add your code here
+function submitData(name, email) { 
+  
+    return fetch('http://localhost:3000/users', { 
+        method: "POST", 
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+        },
+        body: JSON.stringify({ 
+            "name": `${name}`,
+            "email": `${email}`
+        }) 
+    }) 
+        .then(Response => Response.json()) 
+        .then(object => { 
+            let p = document.createElement('p'); 
+            p.textContent = object.id; 
+            document.querySelector('body').appendChild(p); 
+        }) 
+        .catch(error => { 
+            document.querySelector('body').textContent = error; 
+        }) 
+} 
+submitData("Quincy", "Quincymail")
